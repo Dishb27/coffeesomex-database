34de05a214b8ba940c2be7a39e9fea41e3f47126
Dishb27 <dbawanga27@gmail.com>
2026-07-24 11:25:06 +0530

    Commit changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 export const metadata = {
@@ -9,7 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
